f1d0d9171519db7567a066d70ae0212eaf9d454f
Daniel Gomes <me@danielcsgomes.com>
2012-03-07 23:20:34 +0000

    added router, updated index and server to support router

---
+++ BasicExperiments/router.js
@@ -0,0 +1,6 @@
+function route(pathname)
+{
+    console.log("Log the requested pahtname: " + pathname);
+}
+
+exports.route = route;
--- BasicExperiments/server.js
+++ BasicExperiments/server.js
@@ -1,15 +1,23 @@
 var http = require("http");
+var url  = require("url");
 
-function start() {
-  function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+function start(route)
+{
+    function onRequest(request, response)
+    {
+        console.log("Request received.");
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+        pathname = url.parse(request.url).pathname;
+
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
 
 exports.start = start;
